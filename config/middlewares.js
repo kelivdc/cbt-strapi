e772e3bf0288f5fce82fe3a8f30e718862a0b806
Arie Ardiansyah <kelivdc@yahoo.com>
2025-03-08 15:52:46 +0700

    Update CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,33 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::poweredBy',  
+  'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        '*',
+        'http://localhost:1337',
+        'https://admin.misteryoscourse.com',
+        'http://localhost:3000',
+        'https://api.misteryoscourse.com',
+        'https://member.misteryoscourse.com',
+        'http://cbt.misteryoscourse.com:1337',
+        'https://cbt.misteryoscourse.com',
+        'https://misteryoscourse.com',
+        'http://localhost:5173',
+        'http://localhost:4000'
+      ]
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  'strapi::cors',  
 ];
 
 // 'strapi::security',
